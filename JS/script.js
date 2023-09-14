e947acfabd7b0a2be058edee8e456cbfc4dea8e4
Saamasi <145021146+Saamasi@users.noreply.github.com>
2023-09-14 22:03:40 +0700

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,31 @@
+const HasilLuas = document.getElementsById("hasilLuas");
+conts HasilKeliling = document.getElementById("hasilKeliling")
+
+function hitungLuas() {
+    const sisi = parseInt(document.getElementById("luasSisi").value);
+    if (isNaN(sisi) || sisi <= 0 || sisi == "") {
+        alert("Jumlah sisi harus bilangan positif dan tidak boleh kosong atau 0.");
+        return;
+    }
+    const luas = sisi * sisi;
+    HasilLuas.innerHTML =
+    <p>Hasil :</p>
+    <p>${luas} cm2</p>
+    ;
+}
+
+function hitungKeliling() {
+    const sisi = parseInt(document.getElementById("kelilingSisi").value);
+    if (isNaN(sisi) || sisi <= 0 || sisi == "") {
+        alert("Jumlah sisi harus bilangan positif dan tidak boleh kosong atau 0.");
+        return;
+    }
+    const luas = 4 * sisi;
+    HasilKeliling.innerHTML =
+    <p>Hasil :</p>
+    <p>${keliling} cm2</p>
+    ;
+}
+
+document.getElementById("hitungLuas").addEventListener("click", hitungLuas);
+document.getElementById("hitungKeliling").addEventListener("click", hitungKeliling);
